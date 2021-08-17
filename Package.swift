8e0285e30a8442f69af8a0b1fdff411b86e94399
Marcos Ramos Rubio <marcos.ramos-rubio@grandcentrix.net>
2021-08-17 15:34:33 +0200

    Adds type dynamic

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "MarketingCloudSDK",
+            type: .dynamic,
             targets: ["MarketingCloudSDK", "MarketingCloudSDKBundle"]),
     ],
     dependencies: [
